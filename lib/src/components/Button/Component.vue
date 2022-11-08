f5dc6e0fa90d7bf8a22cc6f144d2759f88912c92
matty260 <mateuszhudzik.dev@gmail.com>
2022-11-08 20:17:21 +0100

    Create basic template for Button component

---
+++ lib/src/components/Button/Component.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    :title="title ?? undefined"
+  >
+
+  </button>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  title: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'VbButton'
+}
+</script>
---
+++ lib/src/components/Button/index.ts
@@ -0,0 +1,1 @@
+export { default as VbButton } from './Component.vue';
